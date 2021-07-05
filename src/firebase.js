8c34ad0a10f1d6f39d6c7cb4c200de8edb3674f8
RAza ALy <74565278+RAzaALy@users.noreply.github.com>
2021-07-05 10:53:19 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,6 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "your api key",
-  authDomain: "messengerclone-d4793.firebaseapp.com",
-  projectId: "messengerclone-d4793",
-  storageBucket: "messengerclone-d4793.appspot.com",
-  messagingSenderId: "36722609367",
-  appId: "1:36722609367:web:94c4253b0348c664e87492",
-  measurementId: "G-6YZD2KC1D7",
+your firebase config
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
